089d4c330ac63e740a95b268993c9c6293951d04
Jayashrikothandapani <jayashrikgokul@gmail.com>
2024-03-07 11:22:11 +0530

    Update

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+//navbar component
+function Navbar() {
+    return (
+      <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>
+            Home | </li>
+          <li><a>Products | </a></li>
+          <li><a>About |</a> </li>
+          <li><a>Contact |</a> </li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Navbar
